98b1a191bccbac7c6a3ff62affff09dbb469c1c4
cmzou <39781351+cmzou@users.noreply.github.com>
2019-07-12 10:46:55 -0400

    moved functions to file and updated visuals with new output

--- PrivateLR_J/README.md
+++ PrivateLR_J/README.md
@@ -1,27 +1,42 @@
 # Data
 
-Data is cleaned using OpenRefine. Currently, the data consists of NC only.
+Data is cleaned using a modified version of yyz's preprocessing.
 
-Deleted rows with "Not applicable" applicant race/sex/ethnicity because it implies that they are corporations.
+Deleted co_applicant, rate_spread, sequence_number, respondent_id, purchaser_type_name, preapproval_name, msamd_name, hoepa_status_name, edit_status_name, and the other applicant_race_name (except for first) columns because they are not relevant, are too granular, or have many missing values.
+
+Deleted population and number_of_1_to_4_family_units columns due to multicollinearity concerns.
+
+Deleted rows with "Not applicable"/"Information not provided" applicant race/sex/ethnicity because it implies that they are corporations and we don't want missing data.
 
 Deleted Multifamily dwelling from property_type_name since all the income fields were blank.
 
-Took natural log of income and loan amount. Clipped loan amount e^4 < x < e^7 for loan amounts that are more relevant to population.
+Deleted rows with denial reason = application incomplete.
+
+Deleted HOEPA loans because there are not many.
+
+One hot encoded action_taken_name to 1 for "loan originated"/"loan purchased"/"loan approved but not accepted" and 0 for "loan denied".
+
+Took natural log of income and loan amount because it looks more normal. Clipped loan amount log(100) < x < log(700) for loan amounts that are more relevant to population.
+
+Took sqrt of minority_population and number_of_owner_occupied_units because it looks more normal.
 
 Removed rows with missing data. 
 
 # Future plans
 
-1. Add in VA, SC, GA data
-2. Use encoded data (for better data sharing)
-3. Average model with more runs
+1. Average model with more runs
+
+# Files
+
+1. `hmda balance.Rmd`: takes hmda data and creates datasets that are balanced in accept/deny and protected/non-protected classes.
+2. `hmda filter for state.ipynb`: filters entire hmda dataset for state
+3. `hmda model.Rmd`: creates model and graphs interesting things. currently only works with encoded data
+4. `hmda results visualization r.Rmd`: uses outputs from scripts to create cool visualizations. some of the plots in `./output/` are created using this
 
 # Scripts
 
-1. `hmda balance.Rmd`: R, takes hmda data and creates datasets that are balanced in accept/deny and protected/non-protected classes.
-2. `hmda filter for state.ipynb`: Python, filters entire hmda dataset for state
-3. `hmda model.Rmd`: creates model and graphs interesting things. currently only works with encoded data
-4. `hmda_privatelr_model_averaging.R`: creates many models and averages them. to be used on commandline
-5. `hmda_privatelr_script.R`: runs k-fold cross validation. to be used on commandline
+1. `hmda_privatelr_model_averaging.R`: creates many models and averages them. to be used on command line
+2. `hmda_privatelr_script.R`: runs k-fold cross validation. to be used on command line
+3. `functions.R`: useful functions for plotting, data filtering, etc. 
 
 Last updated: 7/11/2019
---
+++ PrivateLR_J/scripts/functions.R
@@ -0,0 +1,159 @@
+################
+# 
+# Script of useful functions.
+# 
+################
+
+# --------------
+# Functions primarily used in "hmda model.Rmd"
+# AKA Functions to help with model building
+
+# Initializes the function needed when using custom models in caret's k-fold cross validation
+init_cv <- function() {
+  lr_init <- list(type="Classification",
+                  library="PrivateLR",
+                  loop = NULL)
+  lr_init$parameters <- data.frame(
+    parameter = c("eps", "do.scale", "threshold", "op"),
+    class = c("numeric", "logical", "character", "logical"),
+    label = c("eps", "do.scale", "threshold", "op")
+  )
+  
+  lr_fit <- function(x, y, wts, param, lev, last, weights, classProbs, ...) {
+    
+    d <- as.data.frame(x)
+    d$y <- y
+    
+    # Spaces in column names breaks stuff since by here, the dplr function has one hot encoded everything
+    colnames(d)<-make.names(colnames(d),unique = TRUE)
+    
+    m <- PrivateLR::dplr(
+      y ~ ., 
+      data=d,
+      eps = param$eps,
+      do.scale = param$do.scale,
+      threshold = param$threshold,
+      op = param$op,
+      ...
+    )
+    
+    # Write coefficients etc to file
+    coef <- m$coefficients
+    df <- data.frame(t(coef))
+    df$CIndex <- m$CIndex
+    df <- cbind(df, param)
+    fwrite(df, paste0("model_data.csv"), append=TRUE)
+    
+    return(m)
+  }
+  lr_init$fit <- lr_fit
+  
+  lr_pred <- function(modelFit, newdata, preProc=NULL, submodels=NULL) {
+    PrivateLR::predict.dplr(object = modelFit, data = newdata, type = "class")
+  }
+  lr_init$predict <- lr_pred
+  
+  lr_init$prob <- function(modelFit, newdata, preProc=NULL, submodels=NULL) {
+    PrivateLR::predict.dplr(modelFit, newdata)
+  }
+  
+  # Tuning parameters
+  lr_grid <- function(x, y, len = NULL, search = "grid") {
+    # Create df of all possible combinations
+    ret <- expand.grid(eps = c(0.00001, 0.0001, 0.001, 0.01, 0.1, 1, 10, 100, 1000), 
+                       op = c(T,F), 
+                       threshold = c("0.5", "youden", "topleft"),
+                       do.scale = c(T), stringsAsFactors = FALSE)
+    # If eps=0, op must=F.
+    ret <- rbind(ret, expand.grid(eps = c(0),
+                                  op = c(F),
+                                  threshold = c("0.5", "youden", "topleft"),
+                                  do.scale = c(T), stringsAsFactors = FALSE))
+    
+    return(ret)
+  }
+  lr_init$grid <- lr_grid
+  
+  return(lr_init)
+}
+
+# Define new summary function, also to be used with caret's k-fold cross validation
+summ <- function(data, lev = NULL, model = NULL) {
+  cm <- confusionMatrix(data$pred, data$obs)
+  ret <- c(cm$overall[1:2], 
+           cm$byClass[1:6], 
+           cm$table)
+  names(ret) <- c(names(ret)[1:(length(ret)-4)], "TrueN", "FalseP", "FalseN", "TrueP")
+  return(ret)
+}
+
+# --------------
+
+
+
+# --------------
+# Functions primarily used in "hmda results visualization r.Rmd"
+# AKA Functions to help with plotting
+
+# Function that converts a df that has columns for TN, TP, etc. into
+# a plottable confusion matrix
+# Args: 
+#   data: df
+#   idxs: the indices of the start of the TN, TP, etc. to be added to summ
+#   summ: the dataframe with columns for true and predicted, the dataframe to be added to
+# Returns summ with an extra column with TN, TP, etc. values
+convert_one <- function(data, idxs, summ_out) {
+  summ <- aggregate(data[,idxs], # indices indicate where the TN, TP, etc. are
+                    by = list(data$eps), mean) # group by eps
+  
+  # What is the name of the new column?
+  name <- paste0(substr(colnames(summ)[2], 3, nchar(colnames(summ)[2])))
+  
+  summ$Group.1 <- NULL # assumming that the dataframe has a column that indicates group
+  summ <- t(summ)
+  summ <- c(summ)
+  
+  # Add column
+  summ_out[, name] <- summ
+  
+  return(summ_out)
+}
+
+# Function to plot confusion matrix
+# true: column of 0s and 1s to use as x-axis
+# predicted: column of 0s and 1s to use as y-axis
+#   this is so that when plotted, the values at coordinates (true, predicted) represent values in the cm
+# col: the column name of the values to go in each box
+plot_cm <- function(data, col, true="true", predicted="predicted", title = NULL, 
+                    use_facet=FALSE, facet=NULL, lab = NULL) {
+  # Formatting digits
+  data[, col] <- as.numeric(format(data[[col]], digits=3))
+  
+  # Creating plots
+  if(use_facet) {
+    p <- ggplot(data =  data, mapping = aes_string(x = true, y = predicted)) + 
+      geom_tile(aes_string(fill = col), colour = "white") +
+      geom_text(aes_string(label = col), vjust = 1) +
+      scale_fill_gradient(low = "white", high = "red") +
+      theme_bw() + theme(legend.position = "none")+
+      labs(title = title) + 
+      facet_wrap(formula(paste0(". ~", facet)))
+  } else {
+    p <- ggplot(data =  data, mapping = aes_string(x = true, y = predicted)) + 
+      geom_tile(aes_string(fill = col), colour = "white") +
+      geom_text(aes_string(label = col), vjust = 1) +
+      scale_fill_gradient(low = "white", high = "red") +
+      theme_bw() + theme(legend.position = "none")+
+      labs(title = title) 
+  }
+  if(!is.null(lab)) {
+    p <- p + labs(lab)
+  }
+  return(p)
+}
+
+# --------------
+
+
+
+
